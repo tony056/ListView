6c654d1d9e7e1781610a11a46bfaf9698df68814
tony056 <tony61507@gmail.com>
2014-07-31 20:15:34 +0800

    add arrayAdapter

--- gen/com/example/listview/R.java
+++ gen/com/example/listview/R.java
@@ -1080,7 +1080,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int LinearLayout2=0x7f05003c;
+        public static final int LinearLayout2=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1092,7 +1092,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1111,8 +1111,9 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f05003d;
+        public static final int imageView1=0x7f05003e;
         public static final int listMode=0x7f050001;
+        public static final int listView1=0x7f05003c;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
--- src/com/example/listview/MainActivity.java
+++ src/com/example/listview/MainActivity.java
@@ -4,32 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
+
+	private ListView listView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//1: only text
-		//1: array 1 ~ 8
-		//1: style item1
-		
-		
-		//2: image + text
-		//2: customize item view
-		//2: list of map string & object
-		
-		//3: image + text + web
-		//3: init web activity
-		//3: set manifest
-		//3: webview
-		//3: on item click
-		
-		//4: image + 2 lines text
-		//4: item view
-		//4: hash map string & object
-		
+		listView = (ListView) findViewById(R.id.listView1);
+		String[] data = { "1", "2", "3", "4", "5", "6", "7", "8" };
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, data);
+		listView.setAdapter(arrayAdapter);
+
+		// 1: only text
+		// 1: array 1 ~ 8
+		// 1: style item1
+
+		// 2: image + text
+		// 2: customize item view
+		// 2: list of map string & object
+
+		// 3: image + text + web
+		// 3: init web activity
+		// 3: set manifest
+		// 3: webview
+		// 3: on item click
+
+		// 4: image + 2 lines text
+		// 4: item view
+		// 4: hash map string & object
+
 	}
 
 	@Override
